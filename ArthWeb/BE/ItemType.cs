c28f6bf5d46560b406ebe68d8bfe82d2ceb9b2de
ankitsharmait17 <ankit.sharma.it2017@gmail.com>
2018-07-22 18:16:59 +0530

    project creation

---
+++ ArthWeb/BE/ItemType.cs
@@ -0,0 +1,28 @@
+namespace BE
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("ItemType")]
+    public partial class ItemType
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public ItemType()
+        {
+            ItemMappings = new HashSet<ItemMapping>();
+        }
+
+        public int ItemTypeID { get; set; }
+
+        [Column("ItemType")]
+        [Required]
+        [StringLength(50)]
+        public string ItemType1 { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ItemMapping> ItemMappings { get; set; }
+    }
+}
