40d9030daebce32e63ee74f9a9bc846ed835c12e
ankitsharmait17 <ankit.sharma.it2017@gmail.com>
2018-09-10 23:15:25 +0530

    Added User entity and DAO

---
+++ ArthWeb/BE/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BE
+{
+    public class User
+    {
+        [StringLength(254)]
+        [Key]
+        public String EmailID { get; set; }
+
+        [StringLength(50)]
+        public String Password { get; set; }
+
+        [StringLength(50)]
+        public String FirstName { get; set; }
+
+        [StringLength(50)]
+        public String LastName { get; set; }
+
+        [StringLength(10)]
+        public String Phone { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ ArthWeb/DL/UserDAO.cs
@@ -0,0 +1,18 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public class UserDAO
+    {
+        public User GetUser(string email,string password)
+        {
+            User user = null;
+            return user;
+        }
+    }
+}
